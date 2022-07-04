07f9ef6eaa59091c8b9772d949f72640155bd341
Igor432 <igomar0602@gmail.com>
2022-07-04 22:15:34 +0300

    feat(homework): vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryList =  galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__card"><a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a></li>`;
+    }).join('');
+;
+
+gallery.insertAdjacentHTML('afterbegin', galleryList);
+
+const lightBox = new simpleLightbox('.gallery__item', { captionSelector: 'img', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function timeupdate(data) {
+const timeSec = data.seconds
+localStorage.setItem("videoplayer-current-time", timeSec);
+console.log(data.seconds);
+
+};
+    
+
+const timeUpdate = localStorage.getItem("videoplayer-current-time");
+
+
+
+
+player.setCurrentTime(timeUpdate).then(function(seconds) {
+})
+
+
+
+    player.on('pause', throttle(onPlay, 500));
