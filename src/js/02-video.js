03fef643ec5dc4e7f7b6dac7d32a53ad117fa2bc
Igor432 <igomar0602@gmail.com>
2022-07-10 14:31:22 +0300

    timer updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,34 +2,37 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-  
-
-    const onPlay = (data) => {
-        const timeSec = data.seconds;
-        localStorage.setItem("videoplayer-current-time", timeSec);
-        console.log(data.seconds);
-
-    };
-    
-
-const timeUpdate = localStorage.getItem("videoplayer-current-time");
+const player = new Player(iframe);
 
 
 
 
-player.setCurrentTime(timeUpdate).then(function(seconds) {
+
+function onPlay(data) {
+    console.log(data.seconds)
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+
+player.setCurrentTime(currentTime).then(function (seconds) {
 })
 
 
+/*
+player.on('pause', throttle(onPlay, 500));
 
-    player.on('pause', throttle(onPlay, 500));
+*/
